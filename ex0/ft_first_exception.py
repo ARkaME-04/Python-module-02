7087555b181c1ac2ffb99163dd384ecc465192b0
dexter <rhyanclarck123@gmail.com>
2026-04-23 21:59:46 +0300

    ex0 of module 2

---
+++ ex0/ft_first_exception.py
@@ -0,0 +1,19 @@
+def input_temperature(temp_str: str) -> int:
+    return int(temp_str)
+
+
+def test_temperature() -> None:
+    try:
+        result = input_temperature("25")
+        print(f"Input data is '{result}'\nTemperature is now {result}°C")
+    except ValueError as e:
+        print(f"Caught input_temperature error: {e}")
+    try:
+        result = input_temperature("abc")
+        print(f"Input data is '{result}'\nTemperature is now {result}°C")
+    except ValueError as e:
+        print(f"Caught input_temperature error: {e}")
+
+
+if __name__ == "__main__":
+    test_temperature()
